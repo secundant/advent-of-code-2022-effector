0fd8c9200a6baca21e690d741043d26d727596ce
Dmitry Remezov <mysecundant@gmail.com>
2023-01-08 14:50:02 +0300

    refactor: day 3, part 2

--- src/solutions/day-03/solution-part-2.ts
+++ src/solutions/day-03/solution-part-2.ts
@@ -1,26 +1,16 @@
 import { sum } from '@/shared/math';
-import { chunk, uniq } from '@/shared/std';
-import { $rucksacks, getPriority } from '@/solutions/day-03/solution';
+import { chunk } from '@/shared/std';
+import { $encryptedRucksacks, getPriority } from '@/solutions/day-03/solution';
 
 const elvesInGroup = 3;
-const countOccurrencesReducer = <T extends string>(acc: Record<T, number>, items: T[]) => {
-  items.forEach(value => {
-    acc[value] ??= 0;
-    acc[value]++;
-  });
-  return acc;
-};
 
-export const $badgesTotalPriorities = $rucksacks
-  .map(rucksacks => chunk(rucksacks.map(rucksack => rucksack.flat()).map(uniq), elvesInGroup))
-  .map(groups =>
-    groups
-      .map(rucksacks => {
-        const stats = rucksacks.reduce(countOccurrencesReducer, {});
-        const candidateEntry = Object.entries(stats).find(([_, count]) => count === elvesInGroup);
-
-        return candidateEntry![0];
-      })
+export const $badgesTotalPriorities = $encryptedRucksacks
+  .map(rucksacks =>
+    chunk(rucksacks, elvesInGroup)
+      .map(
+        ([first, ...other]) =>
+          Array.from(first).find(char => other.every(rucksack => rucksack.includes(char)))!
+      )
       .map(getPriority)
   )
   .map(sum);
